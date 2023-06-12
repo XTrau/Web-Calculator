b5df16b569e54e47dd1e2fc175abc43d7589830c
Vildan Khismatullin <khismatullin.vildan@mail.ru>
2023-06-12 13:34:07 +0500

    Code for calculations

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-const screen = document.getElementById('screen')
-let lastSymbol = null
+const screen = document.querySelector('#screen')
 let calcOut = '0'
+let lastSymbol = null
+let action = null
 
 function buttonClick(value) {
-    if (isNaN(value))
+    if (isNaN(value)) {
         addSymbol(value)
+    }
     else
         addNumber(value)
 
     if (calcOut.length > 18)
-        addSymbol('←')
+        calcOut = calcOut.substring(0, calcOut.length - 1)
 
     screen.innerText = calcOut
 }
@@ -30,29 +32,69 @@ function addSymbol(value) {
     switch(value) {
         case 'C':
             calcOut = '0'
+            lastSymbol = null
+            action = null
             break;
         case '←':
+            if (action === calcOut[calcOut.length - 1])
+                action = null
             calcOut = calcOut.substring(0, calcOut.length - 1)
             calcOut = calcOut.length === 0 ? '0' : calcOut
             lastSymbol = calcOut[calcOut.length - 1]
             break;
         case '=':
             doCalc()
-            lastSymbol = null
+            action = null
+            lastSymbol = calcOut[calcOut.length - 1]
             break;
         case '÷':
         case '×':
         case '−':
         case '+':
+            if (action != null && isNaN(lastSymbol) === false) {
+                doCalc()
+                calcOut += value
+                lastSymbol = value
+                action = value
+                break
+            }
+
             if (isNaN(lastSymbol))
                 calcOut = calcOut.substring(0, calcOut.length - 1)
+            
             calcOut += value
             lastSymbol = value
+            action = value
     }
 }
 
 function doCalc() {
-
+    if (action === null)
+        return
+    
+    let indexOfAction = calcOut.indexOf(action);
+    if(calcOut[indexOfAction + 1] === undefined) {
+        return
+    }
+    switch (action) {
+        case '÷':
+            calcOut = calcOut.substring(0, indexOfAction) /
+                calcOut.substring(indexOfAction + 1)
+            break
+        case '×':
+            calcOut = calcOut.substring(0, indexOfAction) *
+                calcOut.substring(indexOfAction + 1)
+            break
+        case '−':
+            calcOut = calcOut.substring(0, indexOfAction) -
+                calcOut.substring(indexOfAction + 1)
+            break
+        case '+':
+            calcOut = Number(calcOut.substring(0, indexOfAction)) +
+                Number(calcOut.substring(indexOfAction + 1))
+            break
+    }
+    calcOut = String(calcOut)
 }
 
 document.querySelector('.calc-buttons').
