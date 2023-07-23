0641d20fe2d9427013297d3bdf4d3bdac8b465c6
Khismatullin Vildan <khismatullin.vildan@mail.ru>
2023-07-23 19:08:42 +0500

    fixes

--- script.js
+++ script.js
@@ -1,101 +1,99 @@
-const screen = document.querySelector('#screen')
-let calcOut = '0'
-let lastSymbol = null
-let action = null
+const screen = document.querySelector('#screen');
+let calcOut = '0';
+let lastSymbol = null;
+let action = null;
 
 function buttonClick(value) {
-    if (isNaN(value)) {
-        addSymbol(value)
-    }
-    else
-        addNumber(value)
+  if (isNaN(value)) {
+    addSymbol(value);
+  } else addNumber(value);
 
-    if (calcOut.length > 17)
-        calcOut = calcOut.substring(0, calcOut.length - 1)
+  if (calcOut.length > 15) calcOut = calcOut.substring(0, calcOut.length - 1);
 
-    screen.innerText = calcOut
+  screen.innerText = calcOut;
 }
 
 function addNumber(value) {
-    lastSymbol = value
-    if (calcOut === '0') {
-        calcOut = value
-        return
-    }
-    calcOut += value
+  lastSymbol = value;
+  if (calcOut === '0') {
+    calcOut = value;
+    return;
+  }
+  calcOut += value;
 }
 
 function addSymbol(value) {
-    if (calcOut === '0')
-        return
+  if (calcOut === '0') return;
 
-    switch(value) {
-        case 'C':
-            calcOut = '0'
-            lastSymbol = null
-            action = null
-            break;
-        case '←':
-            if (action === calcOut[calcOut.length - 1])
-                action = null
-            calcOut = calcOut.substring(0, calcOut.length - 1)
-            calcOut = calcOut.length === 0 ? '0' : calcOut
-            lastSymbol = calcOut[calcOut.length - 1]
-            break;
-        case '=':
-            doCalc()
-            action = null
-            lastSymbol = calcOut[calcOut.length - 1]
-            break;
-        case '÷':
-        case '×':
-        case '−':
-        case '+':
-            if (action != null && isNaN(lastSymbol) === false) {
-                doCalc()
-                calcOut += value
-                lastSymbol = value
-                action = value
-                break
-            }
+  switch (value) {
+    case 'C':
+      calcOut = '0';
+      lastSymbol = null;
+      action = null;
+      break;
+    case '←':
+      if (action === calcOut[calcOut.length - 1]) action = null;
+      calcOut = calcOut.substring(0, calcOut.length - 1);
+      calcOut = calcOut.length === 0 ? '0' : calcOut;
+      lastSymbol = calcOut[calcOut.length - 1];
+      break;
+    case '=':
+      doCalc();
+      action = null;
+      lastSymbol = calcOut[calcOut.length - 1];
+      break;
+    case '÷':
+    case '×':
+    case '−':
+    case '+':
+      if (action != null && isNaN(lastSymbol) === false) {
+        doCalc();
+        calcOut += value;
+        lastSymbol = value;
+        action = value;
+        break;
+      }
 
-            if (isNaN(lastSymbol))
-                calcOut = calcOut.substring(0, calcOut.length - 1)
-            
-            calcOut += value
-            lastSymbol = value
-            action = value
-    }
+      if (isNaN(lastSymbol)) calcOut = calcOut.substring(0, calcOut.length - 1);
+
+      calcOut += value;
+      lastSymbol = value;
+      action = value;
+  }
 }
 
 function doCalc() {
-    if (action === null)
-        return
-    
-    let indexOfAction = calcOut.indexOf(action);
-    if(calcOut[indexOfAction + 1] === undefined) {
-        return
-    }
-    switch (action) {
-        case '÷':
-            calcOut = calcOut.substring(0, indexOfAction) /
-                calcOut.substring(indexOfAction + 1)
-            break
-        case '×':
-            calcOut = calcOut.substring(0, indexOfAction) *
-                calcOut.substring(indexOfAction + 1)
-            break
-        case '−':
-            calcOut = calcOut.substring(0, indexOfAction) -
-                calcOut.substring(indexOfAction + 1)
-            break
-        case '+':
-            calcOut = Number(calcOut.substring(0, indexOfAction)) +
-                Number(calcOut.substring(indexOfAction + 1))
-            break
-    }
-    calcOut = String(calcOut)
+  if (action === null) return;
+
+  let indexOfAction = calcOut.indexOf(action);
+  if (calcOut[indexOfAction + 1] === undefined) {
+    return;
+  }
+  switch (action) {
+    case '÷':
+      calcOut =
+        calcOut.substring(0, indexOfAction) /
+        calcOut.substring(indexOfAction + 1);
+      break;
+    case '×':
+      calcOut =
+        calcOut.substring(0, indexOfAction) *
+        calcOut.substring(indexOfAction + 1);
+      break;
+    case '−':
+      calcOut =
+        calcOut.substring(0, indexOfAction) -
+        calcOut.substring(indexOfAction + 1);
+      break;
+    case '+':
+      calcOut =
+        Number(calcOut.substring(0, indexOfAction)) +
+        Number(calcOut.substring(indexOfAction + 1));
+      break;
+  }
+  calcOut = String(calcOut);
 }
 
-document.querySelector('.calc-buttons').
-addEventListener('click', event => buttonClick(event.target.innerText))
+document
+  .querySelector('.calc-buttons')
+  .addEventListener('click', (event) => buttonClick(event.target.innerText));
